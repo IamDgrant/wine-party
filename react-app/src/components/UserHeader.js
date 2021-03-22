dbbe5b888f8e186c24f9bd99f959e1b239387ef6
Andre Grant <Iamdgrant@me.com>
2021-03-22 00:02:03 -0400

    added search bar but not functional yet

--- react-app/src/components/UserHeader.js
+++ react-app/src/components/UserHeader.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { NavLink, useHistory } from "react-router-dom";
 // import DateTime from "./Date";
 // import Date from "../components/Date";
-import FindHost from "../components/FindHost"
+import FindHost from "../components/FindHost";
 import AddEventModal from "../components/auth/modals/AddEventModal";
 import SearchForm from "../components/forms/SearchHostForm";
 import { seeHost } from "../store/host";
@@ -25,6 +25,7 @@ import "../components/styling/userHeader.css";
 // import LogoutButton from "../components/auth/LogoutButton";
 // import { profileImage } from "../store/session";
 // import "./styling/navBar.css";
+import SearchIcon from "../assets/icons/search-solid.svg";
 
 const UserHeader = () => {
   const history = useHistory();
@@ -52,15 +53,13 @@ const UserHeader = () => {
         console.log("here!!!!!", res);
         if (res.Host === "Not found") {
           message.error(`User ${search} not found`);
-        }
-        else {
+        } else {
           // message.success(`User ${search} added to Event!`);
         }
-        if (res.hosts) history.push("/search")
+        if (res.hosts) history.push("/search");
       }
-      );
+    );
   };
-
 
   useEffect(() => {
     setDate(new Date());
@@ -144,7 +143,59 @@ const UserHeader = () => {
           )}
         </div>
       </div>
-      <div className="background-image"></div>
+      <div className="background-image">
+        <div className="search-form">
+          <form class="form">
+            <input
+              type="search"
+              className="search-input"
+              placeholder="Search name, city, state, postal code..."
+            />
+            <button type="submit" class="search-button">
+              <svg class="submit-button">
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="search"
+                  class="svg-inline--fa fa-search fa-w-16"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+                </svg>
+              </svg>
+            </button>
+            <div class="search-option">
+              <div>
+                <input
+                  name="type"
+                  type="checkbox"
+                  value="type-somm"
+                  id="type-somm"
+                />
+                <label for="type-somm">
+                  <span>Sommelier</span>
+                </label>
+              </div>
+
+              <div>
+                <input
+                  name="type"
+                  type="checkbox"
+                  value="type-mix"
+                  id="type-mix"
+                />
+                <label for="type-mix">
+                  <span>Mixologist</span>
+                </label>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
       <div className="search-container"></div>
     </>
   );
