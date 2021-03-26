834260d87951f6986afa2daa898224d954616981
Andre Grant <Iamdgrant@me.com>
2021-03-26 10:32:56 -0400

    fixed add event btn

--- react-app/src/components/Date.js
+++ react-app/src/components/Date.js
@@ -10,21 +10,21 @@ const DateTime = () => {
     setDate(new Date());
   }, []);
 
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false,
-  };
-  const dayDate = new Date().toLocaleTimeString("en-us", options).split(",");
+  // const options = {
+  //   weekday: "long",
+  //   year: "numeric",
+  //   month: "long",
+  //   day: "numeric",
+  //   hour: "2-digit",
+  //   minute: "2-digit",
+  //   second: "2-digit",
+  //   hour12: false,
+  // };
+  // const dayDate = new Date().toLocaleTimeString("en-us", options).split(",");
 
   return (
     <>
-      <div className="main-date-container flex items-center">
+      <div className="main-date-container">
         {/* <div className="date-countdown-container"> */}
           {/* <div className="today-date text-2xl">
             {dayDate[0]}, {dayDate[1]}
--- react-app/src/components/UserProfile.js
+++ react-app/src/components/UserProfile.js
@@ -129,69 +129,6 @@ const UserProfile = () => {
         </div>
       </Modal>
     </div>
-    // </div>
-    // {showModal ? (
-    //   <>
-    //     <div
-    //       className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none max-w-full"
-    //       // onClick={() => setShowModal(false)}
-    //     >
-    //       <div className="relative w-auto my-6 mx-auto max-w-3xl">
-    //         {/*content*/}
-    //         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-    //           <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
-    //             <h3 className="text-3xl font-semibold">Profile Settings</h3>
-    //             <button
-    //               className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-    //               onClick={() => setShowModal(false)}
-    //             >
-    //               <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-    //                 ×
-    //               </span>
-    //             </button>
-    //           </div>
-    //           {/*body*/}
-    //           <div className="form-container sign-up-container">
-    //             <form encType="multipart/form-data" onSubmit={submit}>
-    //               <input
-    //                 id="myuniqueid"
-    //                 type="file"
-    //                 name="user_file"
-    //                 onChange={handleUpload}
-    //               ></input>
-    //               {/* <label>Upload Photo</label> */}
-    //               {/* <button type="submit"></button> */}
-    //             </form>
-    //             {/*footer*/}
-    //             <div className="flex items-center justify-end p-4 border-t border-solid border-white rounded-b">
-    //               <div className="px-1">
-    //                 <button
-    //                   className="bg-transparent hover:text-red-200 px-4 border border-white hover:border-white rounded form_title"
-    //                   type="button"
-    //                   style={{ transition: "all .15s ease" }}
-    //                   onClick={() => setShowModal(false)}
-    //                 >
-    //                   Cancel
-    //                 </button>
-    //               </div>
-    //               <div>
-    //                 <button
-    //                   className="bg-transparent hover:text-green-200 px-4 border border-white hover:border-transparent rounded"
-    //                   type="submit"
-    //                   style={{ transition: "all .15s ease" }}
-    //                   onClick={submit}
-    //                 >
-    //                   Submit
-    //                 </button>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-    //   </>
-    // ) : null}
   );
 };
 export default UserProfile;
