a73d7ceede7e2b93f9b9557066576edd97d87247
Andre Grant <Iamdgrant@me.com>
2021-04-02 17:54:54 -0400

    added font weight 400

--- react-app/src/components/AccountContent.js
+++ react-app/src/components/AccountContent.js
@@ -10,10 +10,9 @@ const AccountContent = () => {
       <div className="account-title">Account</div>
       <div ClassName="account-info-name">{sessionUser.first_name} {sessionUser.last_name}</div>
       <div ClassName="account-info-email">{sessionUser.email}</div>
-      <div ClassName="account-info-profile"><NavLink to="/profile"></NavLink></div>
+      <div ClassName="account-info-profile"><NavLink to="/profile" style={{fontWeight: "400", color: "rgb(5, 133, 50)"}}>Your Profile</NavLink></div>
       <div className="account-info"></div>
     </div>
   );
 };
-
 export default AccountContent;
---
+++ react-app/src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <di>
+      <h1>Profile</h1>
+    </di>
+  );
+};
+
+export default Profile;
